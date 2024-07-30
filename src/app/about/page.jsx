282ccef4ca8a0c50f1fe78fedc38825e2d64b6a2
Manuel N. Fabri <manuelnfabri@gmail.com>
2024-07-30 22:36:08 +0200

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,33 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2>About Agency</h2>
+        <h1>
+          We create digital ideas that are bigger, bolder, braver and better
+        </h1>
+        <p>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas, flexibility and precision. We want to be the
+          best agency team in the world, delivering value according to our
+          client needs.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <p>10k</p>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <p>10k</p>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <p>10k</p>
+            <p>Years of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
         <Image src="/about.png" alt="" fill />
       </div>
